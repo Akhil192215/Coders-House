825e7cb2dc697cf7cc5284974520ddbf9e8d5c2f
akhil S <akhilspoovathinkal@gmail.com>
2022-12-31 22:43:29 +0530

    otp backend done

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,13 +1,50 @@
+const otpService = require("../services/otp-service");
+const hashService = require("../services/hash-service");
 class AuthController {
-  sendOtp(req, res) {
+  async sendOtp(req, res) {
     //Logic
-    const {phone} = req.body
-    console.log(phone);
-    if(!phone) {
-        res.status(400).json({message:'phone number is required'})
+    const { phone } = req.body;
+    if (!phone) {
+      res.status(400).json({ message: "phone number is required" });
+    }
+    //generate OTP
+    const otp = await otpService.generateOtp();
+    //hash OTP
+    const ttl = 1000 * 60 * 3; //2min
+    const expiry = Date.now() + ttl;
+    const data = `${phone}.${otp}.${expiry}`;
+    const hash = hashService.hashOtp(data);
+    //send OTP
+
+    try {
+      await otpService.sendBySms(phone, otp);
+      res.json({
+        hash: `${hash}.${expiry}`,
+        phone,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "otp sending faild" });
     }
 
-    res.send("otp send ok");
+    // res.json({ hash: hash });
+  }
+
+  verifyOtp(req, res) {
+    //Logic
+    const [otp, hash, phone] = req.body;
+    if (!otp || !hash || !phone) {
+      res.status(400).json({ message: "some error occured" });
+    }
+    const [hashedOtp,expiry] = hash.split('.')
+    const data = `${phone}.${otp}.${expiry}`;
+    const isValid = otpService.verifyOtpService(hashedOtp,data)
+    if(!isValid) {
+      res.status(400).json({message:"OTP is invalid"})
+    }
+    let user;
+    let accessToken;
+    let refreshToken
   }
 }
 
